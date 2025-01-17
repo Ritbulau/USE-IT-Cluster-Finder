6250d009aecd1f06241c0574b50b9f5c046aa55e
Ritbulau <94283288+Ritbulau@users.noreply.github.com>
2025-01-18 00:46:11 +0300

    Упрощение кода
    Убраны классы, код сокращён до 99 строчек (де-факто, на деле их меньше), убран файл с классом и функциями векторов, убраны ВСЕ предупреждения и прочие жалобы от питона, ПОЛНОЕ соответствие стандартам pep-8, мелкие улучшения в коде

--- ClusterFinder.py
+++ ClusterFinder.py
@@ -1,115 +1,98 @@
-from vector import *
-class Cluster:
-    def __init__(self, a, b):
-
-        self.dotsIn = [a, b]
-
-def findClosestDot(a: Vector2, dots):
-    minimal = 99999999
-    id = -1
-    if len(dots) <= 1:
-        return [id, minimal]
-    for dot in range(len(dots)):
-        distance = dist(a, dots[dot])
-        if distance < minimal and a != dots[dot]:
-            minimal = distance
-            id = dot
-    return [id, minimal]
+def get_length(vector):
+    return (vector[0] ** 2 + vector[0] ** 2) ** 0.5
 
 
-def findClosestCluster(a: Vector2, clusters):
-    if len(clusters) == 0:
-        return [-1, 99999999]
-    minimal = 99999999
-    id = -1
-    for cluster in range(len(clusters)):
-        closest = findClosestDot(a, clusters[cluster].dotsIn)
-        if closest[1] < minimal:
-            id = cluster
-            minimal = closest[1]
-    return [id, minimal]
+def dist(a_vec, b_vec):
+    return get_length([b_vec[0] - a_vec[0], b_vec[1] - a_vec[1]])
 
 
+def find_closest_dot(a_vec, all_dots):
+    min_num = float('inf')
+    index = -1
+    if len(all_dots) <= 1:
+        return [id, min_num]
+    for dot_vec in range(len(all_dots)):
+        distance = dist(a_vec, all_dots[dot_vec])
+        if distance < min_num and a_vec != all_dots[dot_vec]:
+            min_num = distance
+            index = dot_vec
+    return [index, min_num]
 
 
-
-
+def find_closest_cluster(a_vec, cls):
+    if len(cls) == 0:
+        return [-1, float('inf')]
+    min_num = float('inf')
+    index = -1
+    for cl in range(len(cls)):
+        closest = find_closest_dot(a_vec, cls[cl])
+        if closest[1] < min_num:
+            index = cl
+            min_num = closest[1]
+    return [index, min_num]
 
 
 initialDots = [
-    Vector2(17, 17),
-    Vector2(16, 16),
-    Vector2(2, 2),
-    Vector2(1, 1),
-    Vector2(0.5, 1),
-    Vector2(-10, -10),
-    Vector2(-9, -8),
-    Vector2(-11.5, -9),
-    Vector2(-9, -9)
+    [17, 17],
+    [16, 16],
+    [2, 2],
+    [1, 1],
+    [0.5, 1],
+    [-10, -10],
+    [-9, -8],
+    [-11.5, -9],
+    [-9, -9]
 ]
 
-clusterMergingBias = int(input("\nEnter cluster merging distance bias (type '-1' for entering fixed number of clusters): "))
+clusterMergingBias = int(
+    input("\nEnter cluster merging distance bias (type '-1' for entering fixed number of clusters): "))
 if clusterMergingBias < 0:
     numberOfClusters = int(input("Enter number of clusters: "))
 else:
     numberOfClusters = 1
-
 
 clusters = []
 dots = initialDots.copy()
 
 while len(dots) > 1:
     dot = dots[0]
-    closestDot = findClosestDot(dot, dots)
-    cluster = findClosestCluster(dot, clusters)
-    if cluster[1] < closestDot[1]:
-        clusters[cluster[0]].dotsIn.append(dot)
+    closest_dot = find_closest_dot(dot, dots)
+    cluster = find_closest_cluster(dot, clusters)
+    if cluster[1] < closest_dot[1]:
+        clusters[cluster[0]].append(dot)
         dots.remove(dot)
-
     else:
-        clusters.append(Cluster(dots[closestDot[0]], dot))
-        dots.pop(closestDot[0])
+        clusters.append([dots[int(closest_dot[0])], dot])
+        dots.pop(int(closest_dot[0]))
         dots.remove(dot)
 
 if len(dots) == 1 and len(clusters) > 0:
-    clusters[findClosestCluster(dots[0], clusters)[0]].dotsIn.append(dots[0])
+    clusters[find_closest_cluster(dots[0], clusters)[0]].append(dots[0])
     dots.clear()
 
-print("\n")
-
-
-while (len(clusters) > numberOfClusters or clusterMergingBias >= 0) and len(clusters) > 1:
-
+minimalCl = float('inf')
+while (len(clusters) > numberOfClusters or (0 <= clusterMergingBias < minimalCl)) and len(clusters) > 1:
     idA = -1
     idB = -1
-
-    minimalCl = 999999
-
     for a in range(len(clusters)):
         for b in range(len(clusters)):
             if a != b:
-                minimal = 999999
-                for dot in clusters[a].dotsIn:
-                    closestDot = findClosestDot(dot, clusters[b].dotsIn)
+                minimal = float('inf')
+                for dot in clusters[a]:
+                    closestDot = find_closest_dot(dot, clusters[b])
                     if closestDot[1] < minimal:
                         minimal = closestDot[1]
                 if minimal < minimalCl:
                     idA = a
                     idB = b
                     minimalCl = minimal
-
-    if clusterMergingBias >= 0 and minimalCl > clusterMergingBias:
-        break
-
     if idA != -1:
-        dotsA = clusters[idA].dotsIn.copy()
+        dotsA = clusters[idA].copy()
         for dot in dotsA:
-            clusters[idB].dotsIn.append(dot)
+            clusters[idB].append(dot)
         clusters.pop(idA)
-
 
 for i in range(len(clusters)):
     print(f"\nCluster number {i + 1}:")
-
-    for dot in clusters[i].dotsIn:
-        print("\t", dot.x, dot.y)
+    for dot in clusters[i]:
+        print("\t", dot[0], dot[1])
